66eee998783426c251b897d0dc3a512657eed90c
Phyton NF <Phyton NF@DESKTOP-D0QSO6V>
2019-06-04 10:55:53 +0700

    Tes ekspor ke github

---
+++ src/testing/Aritmatika.java
@@ -0,0 +1,17 @@
+
+package testing;
+
+
+public class Aritmatika extends Testing{
+    double c;
+    
+    public void penjumlahan(int a){
+        c = a + a;
+        System.out.println("Hasil penjumlahan\t:"+c);
+    }
+
+    public void perkalian(int a){
+        c = a * a;
+        System.out.println("Hasil penjumlahan\t:"+c);
+    }    
+}
