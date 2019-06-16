4a4d7d2b17032469dfa61bf1161c7428aefa7a26
melininkonadia <49273707+melininkonadia@users.noreply.github.com>
2019-06-16 22:46:45 +0700

    Add files via upload

---
+++ src/Driver/Controller.java
@@ -0,0 +1,11 @@
+
+package Controller1;
+
+import InterfaceView.PanelInput;
+
+
+public interface Controller {
+    public void saveBtn (PanelInput input);
+    public void editBtn (PanelInput input);    
+    public void deleteBtn (PanelInput input);
+}
